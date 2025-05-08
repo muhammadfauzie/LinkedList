a0827127d2e096db92141a32e8077a4edee1c4ca
fauzie primarka <fauzieprimarka@gmail.com>
2025-05-08 19:49:38 +0700

    traverse dan penampilan semua data didalam list

--- linkList.cpp
+++ linkList.cpp
@@ -91,4 +91,23 @@ public:
         return;
     }
 
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+                cout << "\nData didalam list adalah:\n";
+                Node *currentNode = START;
+                while (currentNode != NULL)
+                {
+                    cout << currentNode->noMhs << endl;
+                    currentNode = currentNode->next;
+                }
+                cout << endl;
+        }
+    }
+
    
