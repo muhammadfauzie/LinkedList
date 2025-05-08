dc7333e4d461097de29907781150118f05540efb
fauzie primarka <fauzieprimarka@gmail.com>
2025-05-08 19:50:46 +0700

    struktur menu utama di fungsi main

--- linkList.cpp
+++ linkList.cpp
@@ -110,4 +110,35 @@ public:
         }
     }
 
-   
+    int main()
+    {
+        List mhs;
+        int nim;
+        char ch;
+        while (1)
+        {
+            cout << "Menu" <<endl;
+            cout << endl;
+            cout << "1. Menambahkan data kedalam list" << endl;
+            cout << "2. Menghapus data dari dalam list" << endl;
+            cout << "3. Menampilkan semua data didalam list" << endl;
+            cout << "4. Mencari data dalam list" << endl;
+            cout << "5. Keluar" << endl;
+            cout << endl
+                 << "Masukkan pilihan (1-5): ";
+            cin >> ch;
+            switch (ch)
+            {
+                case '1':
+                {
+                     mhs.addNode();
+                    break;
+                }
+
+                case '2': 
+              
+            }
+        }
+
+    }
+};
